00a4d7e4a34bf545ca50610f76e768299dca0e07
eduardoaugustopimentel <dusx68@gmail.com>
2023-11-16 22:00:39 -0300

    os aMIGOS SÃO tUDO

---
+++ JavaScript/Inicio/trabalhandocomdados/script01.js
@@ -0,0 +1,17 @@
+var fabricanteCarro = "renault"
+var modeloCarro = "logan"
+var precoCarro = 16000
+var anoCarro = 2018
+
+console.log("teste")
+console.log("Carros")
+console.log(fabricanteCarro)
+console.log(modeloCarro)
+console.log(precoCarro)
+console.log(anoCarro)
+
+// modeloCarro = "sandero"
+console.log("fabricante: " + fabricanteCarro + ", modelo: " + modeloCarro + ", preco: " + precoCarro + ", ano: " + anoCarro)
+modeloCarro = "clio"
+
+console.log(modeloCarro)
