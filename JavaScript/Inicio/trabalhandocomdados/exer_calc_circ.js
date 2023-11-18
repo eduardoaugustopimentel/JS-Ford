a17af1d201d046ee141a5258dbc1cd2ac03435fc
Du <dusx68@gmail.com>
2023-11-17 21:54:31 -0300

    criei os aquivos exercicio001.js e condicionais.js

---
+++ JavaScript/Inicio/trabalhandocomdados/exer_calc_circ.js
@@ -0,0 +1,9 @@
+// Calculando a área de uma circunferência
+ const PI = 3.14
+ const raioCirculo = 16
+
+ // processamento de dados
+ const resultadoRaio = PI * (raioCirculo * raioCirculo)
+ 
+ // saída de dados
+ console.log(resultadoRaio + " cm²");
